74f0efd7dbed137e6f1d816777b1b1bc158ddecc
Woody Kromar <wkromar12@gmail.com>
2020-12-11 15:21:54 -0600

    adding buttons, input fields and starting docReady, handleReady functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 console.log("js");
+
+$(document).ready(handleReady);
+
+function handleReady() {
+  console.log("jquery is ready");
+
+  //render items to DOM
+  renderToDom();
+
+  //click listeners
+  $("#submitButton").on("click", handleClick);
+  $("#deleteButton").on("click", handleClick);
+}
