fae17e2fc24f0619271d6cb08106b559503390c9
Woody Kromar <wkromar12@gmail.com>
2020-12-11 15:53:30 -0600

    renderToDom function complete

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ console.log("js");
 
 $(document).ready(handleReady);
 
+let infoBank = [];
+
 function handleReady() {
   console.log("jquery is ready");
 
@@ -11,4 +13,20 @@ function handleReady() {
   //click listeners
   $("#submitButton").on("click", handleClick);
   $("#deleteButton").on("click", handleClick);
+}
+
+function renderToDom() {
+  //make sure the DOM is empty
+  $("#foodList").empty();
+  //append everything in the array, append them to the table
+  for (let info of infoBank) {
+    $("#infoDisplay").append(`
+      <tr class= "info">
+      <td>${info.firstNameInput}</td>
+      <td>${info.lastNameInput}</td>
+      <td>${info.idNumberInput}</td>
+      <td>${info.jobTitleInput}</td>
+      <td>${info.annualSalaryInput}</td>
+      </tr>`);
+  }
 }
